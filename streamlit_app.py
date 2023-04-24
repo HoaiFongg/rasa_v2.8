efce43b0fc6ab0c65923eb46b662db5dc524becb
HoaiFongg <hoaifongg@gmail.com>
2023-04-24 11:30:14 +0700

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,23 @@
 import streamlit as st
 import rasa.shared.utils.io
 
-from rasa.cli import run
+from rasa.cli.utils import get_validated_path
+from rasa.engine import (
+    utils,
+    Engine,
+    Loader,
+)
+from rasa.engine.graph import ExecutionContext
+from rasa.engine.storage import (
+    ModelStorage,
+    Resource,
+    ResourceNotFound,
+)
+from rasa.engine.runner import (
+    MissingRunDependencyException,
+    run_in_worker_thread,
+)
+from rasa.engine.tracker_store import TrackerStore
 from rasa.model import get_model
 from rasa.core.agent import Agent
 from rasa.core.interpreter import RasaNLUInterpreter
